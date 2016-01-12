78d925fb99cf157e911cf571ca827306e0783caf
Btlknajib <boutellaka.najib@laposte.net>
2016-01-12 10:08:20 +0100

    presentation of plugin from jquery called elevatezoom

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$("#zoom_01").elevateZoom();
+
+$("#zoom_02").elevateZoom({ zoomType : "inner", cursor: "crosshair" });
+
+$("#zoom_03").elevateZoom({
+  zoomType				: "lens",
+  lensShape : "round",
+  lensSize    : 200
+});
